31e66c6c219e7711d4cce3c4c2d73f1093331567
jljones23 <78108791+jljones23@users.noreply.github.com>
2021-10-15 13:40:51 -0500

    Delete create-loganalyticsworkspace.ps1

--- create-loganalyticsworkspace.ps1
+++
@@ -1,8 +0,0 @@
-#creating a log analytics workspace
-
-$ResourceGroup="UALR-jaylonjones-rg"
-$Workspacename="log-analytics-jaylon"
-$Location="eastus
-
-#Create the workspace
-New-AzOperationalInsightsWorkspace-Location$Location-Name$WorkspaceName-Jones Standard-ResourceGroupName $ResourceGroup

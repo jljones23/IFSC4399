57aa3a912d918fea76646fcc6777b8f091e59ade
jljones23 <78108791+jljones23@users.noreply.github.com>
2021-10-19 15:14:23 -0500

    Delete createautomationaccount.ps1

--- createautomationaccount.ps1
+++
@@ -1,3 +0,0 @@
-#Create Azure Automation Account in your Azure resource groups.
-$ResourceGroup = "UALR-Jaylon-Jones-rg"
-New-AzureRmAutomationAccount -Name "Automation-jaylon" -Location "East US" -ResourceGroupName $ResourceGroup
